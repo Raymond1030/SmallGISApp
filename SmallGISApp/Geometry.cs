d2e40b84bf912cad66afeb50878885662b3fb822
Raymond1030 <loho1030@gmail.com>
2023-09-19 00:50:23 +0800

    添加项目文件。

---
+++ SmallGISApp/Geometry.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+using System.Drawing;
+
+namespace SmallGISApp
+{
+    internal class Geometry
+    {
+        public int id { get; set; }
+        public string color { get; set; }
+    }
+    //点类 point class
+    internal class Point:Geometry
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public Point(double p_x, double p_y)
+        {
+            this.x = p_x;
+            this.y = p_y;
+        }
+        public void DrawPoint(Canvas canvas, double radius)
+        {
+            Ellipse Drawpoint = new Ellipse
+            {
+                Fill = Brushes.Black,
+                Width = radius * 2,
+                Height = radius * 2
+            };
+
+            Canvas.SetLeft(Drawpoint, this.x - radius);
+            Canvas.SetTop(Drawpoint, this.y - radius);
+
+            canvas.Children.Add(Drawpoint);
+        }
+    }
+    //多点类 multi point class
+    internal class MultiPoint : Geometry
+    {
+
+        public List<Point> m_multiPoint { get; set; }
+        public MultiPoint()
+        {
+            m_multiPoint = new List<Point>();
+        }
+        public void PushPoint(Point P)
+        {
+            m_multiPoint.Add(P);
+        }
+        public void DrawPoint(Point P)
+        {
+        }
+    }
+    //线类 line class
+    internal class Line:Geometry
+    {
+        //线 起点终点
+        public List<Point> m_Line  { get; set; }
+        public Line()
+        {
+           m_Line = new List<Point>();
+        }
+        public void DrawLine(Canvas canvas)
+        {
+            for (int i = 0; i+ 1< m_Line.Count; i++)
+            {
+                LineGeometry myLineGeometry = new LineGeometry();
+                myLineGeometry.StartPoint = new System.Windows.Point(m_Line[i].x, m_Line[i].y);
+                myLineGeometry.EndPoint = new System.Windows.Point(m_Line[i+1].x, m_Line[i+1].y);
+
+                Path myPath = new Path();
+                myPath.Stroke = Brushes.Black;
+                myPath.StrokeThickness = 1;
+                myPath.Data = myLineGeometry;
+
+                canvas.Children.Add(myPath);
+            }
+        
+        }
+    }
+    //多线类 multi line class
+    class MultiLine :Geometry
+    {
+        public List<Line> m_multiLine { get; set; }
+        public MultiLine()
+        {
+            m_multiLine = new List<Line>();
+        }
+        public void PushLine(Line L)
+        {
+            m_multiLine.Add(L);
+        }
+    }
+    //面类 polygon class
+    internal class Polygon:Geometry
+    { 
+        public List<Point> m_polygon { get; set; }
+        public Polygon()
+        {
+            m_polygon = new List<Point>();
+        }
+        public void PushRingPoint(Point P)
+        //添加顶点
+        {
+            m_polygon.Add(P);
+        }
+    }
+    //多面类 multi polygon class
+    internal class MultiPolygon:Geometry
+    {
+        public List<Polygon> m_multiPolygon { get; set; }
+        public MultiPolygon()
+        {
+            m_multiPolygon = new List<Polygon>();
+        }
+        public void PushPolygon(Polygon P)
+        //添加面
+        {
+
+            m_multiPolygon.Add(P);
+        }
+    }
+
+}
---
+++ SmallGISApp/MouseDraw.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmallGISApp
+{
+    /// <summary>
+    /// MouseDraw.xaml 的交互逻辑
+    /// </summary>
+    public partial class MouseDraw : Window
+    {
+        bool isDrawingEnabled = false;//是否开始画图
+
+        //画线
+        private bool isDrawingLine = false; // 是否处于绘制线条的模式
+        Point? currentpoint = null;//记录目前最后一个点
+        Line currentline = new Line();//记录每条线的轨迹
+        Path temPath = new Path();//在鼠标划动过程 线的橡皮筋
+        
+        //图层 记录画好的点、线、面
+        MultiPoint save_Point=new MultiPoint();
+        MultiLine save_Line = new MultiLine();
+        MultiPolygon save_Polygon = new MultiPolygon();
+
+
+        public MouseDraw()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseDrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawingEnabled = !isDrawingEnabled; // Toggle the drawing state
+
+            // Change the button content based on the drawing state
+            if (isDrawingEnabled)
+            {
+                (sender as Button).Content = "Stop Drawing";
+            }
+            else
+            {
+                (sender as Button).Content = "Start Drawing";
+            }
+        }
+
+        private void MousedrawingCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!isDrawingEnabled)
+                return; // Exit if drawing is not enabled
+
+            //1. 鼠标画点
+            if (rbPoint.IsChecked==true)         
+            {   
+                // Get the mouse click position
+                double winX = e.GetPosition(MousedrawingCanvas).X;
+                double winY = e.GetPosition(MousedrawingCanvas).Y;
+                //画点按钮
+                Point m_p = new Point(winX, winY);
+                m_p.DrawPoint(MousedrawingCanvas, 5);
+                //图层记录点
+                save_Point.m_multiPoint.Add(m_p);
+            }
+            //2. 鼠标画线
+            else if (rbLine.IsChecked==true)
+            {
+                // Get the mouse click position
+                double winX = e.GetPosition(MousedrawingCanvas).X;
+                double winY = e.GetPosition(MousedrawingCanvas).Y;
+                currentpoint = new Point(winX, winY);
+                if (e.LeftButton == MouseButtonState.Pressed && !isDrawingLine)
+                {
+                    
+                    currentline.m_Line.Add(currentpoint);
+                    isDrawingLine = true;
+
+                }
+                else if (e.LeftButton == MouseButtonState.Pressed && isDrawingLine)
+                {
+                    // 更新线条的结束位置
+                    currentpoint.x = e.GetPosition(MousedrawingCanvas).X;
+                    currentpoint.y = e.GetPosition(MousedrawingCanvas).Y;
+                    // 添加到线中
+                    currentline.m_Line.Add(currentpoint);
+                    currentline.DrawLine(MousedrawingCanvas);
+                }
+                else if (e.RightButton == MouseButtonState.Pressed && isDrawingLine)
+                {
+                    // 右击鼠标结束绘制线条
+                    currentline = new Line();
+                    isDrawingLine = false;
+                    if (temPath != null)
+                    {
+                        MousedrawingCanvas.Children.Remove(temPath);
+                    }    
+                    save_Line.m_multiLine.Add(currentline);
+                 
+                }
+
+            }
+            //3. 鼠标画面
+            else if (rbPolygon.IsChecked == true)
+            {
+
+            }
+
+        }
+        private void MousedrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (rbLine.IsChecked == true && isDrawingLine && currentline != null)
+            {
+                double winX = e.GetPosition(MousedrawingCanvas).X;
+                double winY = e.GetPosition(MousedrawingCanvas).Y;
+                LineGeometry myLineGeometry = new LineGeometry();
+                myLineGeometry.StartPoint = new System.Windows.Point(currentpoint.x, currentpoint.y);
+                myLineGeometry.EndPoint = new System.Windows.Point(winX, winY);
+
+                if (!MousedrawingCanvas.Children.Contains(temPath))
+                {
+                    temPath.Stroke = Brushes.Black;
+                    temPath.StrokeThickness = 1;
+                    MousedrawingCanvas.Children.Add(temPath);
+                }
+
+                temPath.Data = myLineGeometry;
+            }
+        }
+
+
+    }
+}
