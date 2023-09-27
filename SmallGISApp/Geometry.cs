547d758bdefc1487e17037dad7eab92fdaf12c5c
Raymond1030 <loho1030@gmail.com>
2023-09-27 15:26:10 +0800

    添加项目文件。

---
+++ SmallGISApp/Geometry.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+using System.Drawing;
+using System.Windows.Documents;
+
+
+namespace SmallGISApp
+{
+    internal class Geometry
+    {
+        public double MaxY = -9999;
+        public double MaxX = -9999;
+        public double MinY = 9999;
+        public double MinX = 9999;
+        public int id { get; set; }
+        public System.Windows.Media.Color color { get; set; }//颜色
+        public string attribute { get; set; }//属性
+     
+        public void draw()
+        {
+
+        }
+    }
+    //点类 point class
+    internal class Point:Geometry
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        //点半径
+        public double radius { get; set; }
+        public Point()
+        {
+    
+        }
+        public Point(double p_x, double p_y)
+        {
+            this.x = p_x;
+            this.y = p_y;
+        }
+        public void Draw(Canvas canvas)//点绘画 Draw重载
+        {
+            Ellipse Drawpoint = new Ellipse
+            {
+                //Fill = Brushes.Black,
+                Fill = new SolidColorBrush(this.color), // 使用 SolidColorBrush
+                Width = this.radius * 2,
+                Height = this.radius * 2
+            };
+
+            Canvas.SetLeft(Drawpoint, this.x - this.radius);
+            Canvas.SetTop(Drawpoint, this.y - this.radius);
+
+            canvas.Children.Add(Drawpoint);
+        }
+    }
+    //多点类 multi point class
+    internal class MultiPoint : Geometry
+    {
+
+        public List<Point> m_multiPoint { get; set; }
+        public MultiPoint()
+        {
+            m_multiPoint = new List<Point>();
+        }
+        public void PushPoint(Point P)
+        {
+            m_multiPoint.Add(P);
+        }
+        public void DrawPoint(Point P)
+        {
+        }
+    }
+    //线类 line class
+    internal class Line:Geometry
+    {
+        //线 起点终点
+        public List<Point> m_Line  { get; set; }
+        public Line()
+        {
+           m_Line = new List<Point>();
+        }
+
+        //线宽
+        public double width { get; set; }
+        public void Draw(Canvas canvas)//线绘画 Draw重载
+        {
+            for (int i = 0; i+ 1< m_Line.Count; i++)
+            {
+                //遍历一下box
+                if (m_Line[i].x > MaxX)
+                    MaxX = m_Line[i].x;
+                if (m_Line[i].y > MaxY)
+                    MaxY = m_Line[i].y;
+                if (m_Line[i].x < MinX)
+                    MinX = m_Line[i].x;
+                if (m_Line[i].y < MinY)
+                    MinY = m_Line[i].y;
+
+                LineGeometry myLineGeometry = new LineGeometry();
+                myLineGeometry.StartPoint = new System.Windows.Point(m_Line[i].x, m_Line[i].y);
+                myLineGeometry.EndPoint = new System.Windows.Point(m_Line[i+1].x, m_Line[i+1].y);
+
+                Path myPath = new Path();
+                //myPath.Stroke = Brushes.Black;
+                myPath.Stroke = new SolidColorBrush(this.color);
+                myPath.StrokeThickness = width;
+                myPath.Data = myLineGeometry;
+
+                canvas.Children.Add(myPath);
+            }
+            //遍历一下box
+            //遍历一下box
+            if (m_Line[m_Line.Count-1].x > MaxX)
+                MaxX = m_Line[m_Line.Count - 1].x;
+            if (m_Line[m_Line.Count - 1].y > MaxY)
+                MaxY = m_Line[m_Line.Count - 1].y;
+            if (m_Line[m_Line.Count - 1].x < MinX)
+                MinX = m_Line[m_Line.Count - 1].x;
+            if (m_Line[m_Line.Count - 1].y < MinY)
+                MinY = m_Line[m_Line.Count - 1].y;
+
+        }
+    }
+    //多线类 multi line class
+    class MultiLine :Geometry
+    {
+        public List<Line> m_multiLine { get; set; }
+        public MultiLine()
+        {
+            m_multiLine = new List<Line>();
+        }
+        public void PushLine(Line L)
+        {
+            m_multiLine.Add(L);
+        }
+    }
+    //面类 polygon class
+    internal class Polygon:Geometry
+    { 
+        public List<Point> m_polygon { get; set; }
+        public System.Windows.Media.Color paintColor { get; set; }//颜色
+        public double width { get; set; }//线宽
+        public Polygon()
+        {
+            m_polygon = new List<Point>();
+        }
+        public void PushRingPoint(Point P)
+        //添加顶点
+        {
+            m_polygon.Add(P);
+        }
+        public void Draw(Canvas canvas, bool IsFill )//面绘画 Draw重载
+        {
+         
+            if (!IsFill)
+            {
+                // IsFill==false 画多边形线
+                for (int i = 0; i + 1 < m_polygon.Count; i++)
+                {
+                    //遍历一下box
+                    if (m_polygon[i].x > MaxX)
+                        MaxX = m_polygon[i].x;
+                    if (m_polygon[i].y > MaxY)
+                        MaxY = m_polygon[i].y;
+                    if (m_polygon[i].x < MinX)
+                        MinX = m_polygon[i].x;
+                    if (m_polygon[i].y < MinY)
+                        MinY = m_polygon[i].y;
+
+                    LineGeometry myLineGeometry = new LineGeometry();
+                    myLineGeometry.StartPoint = new System.Windows.Point(m_polygon[i].x, m_polygon[i].y);
+                    myLineGeometry.EndPoint = new System.Windows.Point(m_polygon[i + 1].x, m_polygon[i + 1].y);
+                    Path myPath = new Path();
+                    myPath.Stroke = new SolidColorBrush(this.color);
+                    //myPath.Stroke = Brushes.Black;
+                    myPath.StrokeThickness = width;
+                    myPath.Data = myLineGeometry;
+                    canvas.Children.Add(myPath);
+                }
+                //遍历一下box
+                if (m_polygon[m_polygon.Count-1].x > MaxX)
+                    MaxX = m_polygon[m_polygon.Count - 1].x;
+                if (m_polygon[m_polygon.Count - 1].y > MaxY)
+                    MaxY = m_polygon[m_polygon.Count - 1].y;
+                if (m_polygon[m_polygon.Count - 1].x < MinX)
+                    MinX = m_polygon[m_polygon.Count - 1].x;
+                if (m_polygon[m_polygon.Count - 1].y < MinY)
+                    MinY = m_polygon[m_polygon.Count - 1].y;
+
+            }
+            else if (IsFill)
+            {
+                // IsFill==true 多边形涂色
+                // Create solid brush.
+                //SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+                System.Windows.Shapes.Polygon newPolygon = new System.Windows.Shapes.Polygon();
+                List<System.Windows.Point>System_points= new List<System.Windows.Point>();
+                for (int i = 0; i < m_polygon.Count; i++)
+                {
+                    //遍历一下box
+                    if (m_polygon[i].x > MaxX)
+                        MaxX = m_polygon[i].x;
+                    if (m_polygon[i].y > MaxY)
+                        MaxY = m_polygon[i].y;
+                    if (m_polygon[i].x < MinX)
+                        MinX = m_polygon[i].x;
+                    if (m_polygon[i].y < MinY)
+                        MinY = m_polygon[i].y;
+                    System.Windows.Point s_P = new System.Windows.Point(m_polygon[i].x, m_polygon[i].y);
+                    System_points.Add(s_P);
+                }
+                newPolygon.Points = new PointCollection(System_points);
+                //newPolygon.Stroke = Brushes.Black;
+                //线颜色
+                newPolygon.Stroke = new SolidColorBrush(this.color);
+                //线宽
+                newPolygon.StrokeThickness = width;
+                //newPolygon.Fill = Brushes.LightBlue;  
+                //设置填充颜色
+                newPolygon.Fill = new SolidColorBrush(this.paintColor);
+                canvas.Children.Add(newPolygon);
+            }
+        }
+    }
+    //多面类 multi polygon class
+    internal class MultiPolygon:Geometry
+    {
+        public List<Polygon> m_multiPolygon { get; set; }
+        public MultiPolygon()
+        {
+            m_multiPolygon = new List<Polygon>();
+        }
+        public void PushPolygon(Polygon P)
+        //添加面
+        {
+
+            m_multiPolygon.Add(P);
+        }
+    }
+
+}
---
+++ SmallGISApp/MouseDraw.xaml.cs
@@ -0,0 +1,478 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmallGISApp
+{
+    /// <summary>
+    /// MouseDraw.xaml 的交互逻辑
+    /// </summary>
+    
+    public partial class MouseDraw : Window
+    {
+        //平移
+        int Temp;
+        private bool isDragging = false;
+        Point lastPosition = new Point();
+
+        bool isDrawingEnabled = false;//是否开始画图
+
+        //画线 面
+        Point currentPoint = new Point();//记录目前最后一个点
+
+        private bool isDrawingLine = false; // 是否处于绘制线条的模式
+        Line currentLine = new Line();//记录每条线的轨迹
+
+        private bool isDrawingPolygon = false; // 是否处于绘制面的模式
+        Polygon currentPolygon = new Polygon();//记录每个面的点轨迹
+
+        //橡皮筋 线
+        Path temPath = new Path();//在鼠标划动过程 线的橡皮筋
+        
+        //图层 记录画好的点、线、面
+        MultiPoint save_Point=new MultiPoint();
+        MultiLine save_Line = new MultiLine();
+        MultiPolygon save_Polygon = new MultiPolygon();
+
+
+        public MouseDraw()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void MouseDrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawingEnabled = !isDrawingEnabled; // Toggle the drawing state
+
+            // Change the button content based on the drawing state
+            if (isDrawingEnabled)
+            {
+                (sender as Button).Content = "Stop Drawing";
+            }
+            else
+            {
+                (sender as Button).Content = "Start Drawing";
+            }
+        }
+
+        private void MousedrawingCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isDragging == true)
+            {
+                isDrawingEnabled = false;
+                // 获取鼠标点击的位置，并记录下来以便后续计算平移量
+                lastPosition.x = e.GetPosition(MousedrawingCanvas).X;
+                lastPosition.y = e.GetPosition(MousedrawingCanvas).Y;
+
+                // 开始捕获鼠标，以便在拖动时能够持续更新位置信息
+                MousedrawingCanvas.CaptureMouse();
+
+            }
+
+
+            //1. 鼠标画点
+            if (Item_Point.IsSelected == true && isDrawingEnabled == true)
+            {
+                // Get the mouse click position
+                double winX = e.GetPosition(MousedrawingCanvas).X;
+                double winY = e.GetPosition(MousedrawingCanvas).Y;
+                //画点按钮
+                Point m_p = new Point(winX, winY);
+                //画点的半径 由拖动栏控制数值
+                m_p.radius = Size.Value;
+                //画点的颜色 由颜色选择器控制
+                m_p.color = (Color)DrawcolorPicker.SelectedColor;
+                m_p.Draw(MousedrawingCanvas);
+                //图层记录点
+                save_Point.m_multiPoint.Add(m_p);
+            }
+            //2. 鼠标画线
+            else if (Item_Line.IsSelected == true && isDrawingEnabled == true)
+            {
+                // Get the mouse click position
+                double winX = e.GetPosition(MousedrawingCanvas).X;
+                double winY = e.GetPosition(MousedrawingCanvas).Y;
+                currentPoint = new Point(winX, winY);
+                if (e.LeftButton == MouseButtonState.Pressed && !isDrawingLine)
+                {
+
+                    currentLine.m_Line.Add(currentPoint);
+                    isDrawingLine = true;
+
+                }
+                else if (e.LeftButton == MouseButtonState.Pressed && isDrawingLine)
+                {
+                    // 更新线条的结束位置
+                    currentPoint.x = e.GetPosition(MousedrawingCanvas).X;
+                    currentPoint.y = e.GetPosition(MousedrawingCanvas).Y;
+                    // 添加到线中
+                    currentLine.m_Line.Add(currentPoint);
+                    // 画线颜色
+                    currentLine.color = (Color)DrawcolorPicker.SelectedColor;
+                    // 画线宽
+                    currentLine.width = Size.Value;
+                    currentLine.Draw(MousedrawingCanvas);
+                }
+                else if (e.RightButton == MouseButtonState.Pressed && isDrawingLine)
+                {
+                    // 右击鼠标结束绘制线条
+                    // 保存线条到多线图层中
+                    save_Line.PushLine(currentLine);
+
+                    // 去掉橡皮筋线条
+                    if (temPath != null)
+                    {
+
+                        MousedrawingCanvas.Children.Remove(temPath);
+                    }
+                    //重新初始化
+                    currentLine = new Line();
+                    isDrawingLine = false;
+                }
+
+            }
+            //3. 鼠标画面
+            else if (Item_Polygon.IsSelected == true && isDrawingEnabled == true)
+            {
+                // Get the mouse click position
+                double winX = e.GetPosition(MousedrawingCanvas).X;
+                double winY = e.GetPosition(MousedrawingCanvas).Y;
+                currentPoint = new Point(winX, winY);
+                if (e.LeftButton == MouseButtonState.Pressed && !isDrawingPolygon)
+                {
+
+                    currentPolygon.m_polygon.Add(currentPoint);
+                    isDrawingPolygon = true;
+
+                }
+                else if (e.LeftButton == MouseButtonState.Pressed && isDrawingPolygon)
+                {
+                    // 更新线条的结束位置
+                    currentPoint.x = e.GetPosition(MousedrawingCanvas).X;
+                    currentPoint.y = e.GetPosition(MousedrawingCanvas).Y;
+                    // 添加到线中
+                    currentPolygon.m_polygon.Add(currentPoint);
+                    // 画线的颜色
+                    currentPolygon.color = (Color)DrawcolorPicker.SelectedColor;
+                    // 画多边形的颜色
+                    currentPolygon.paintColor = (Color)PaintcolorPicker.SelectedColor;
+                    // 画多边形线宽
+                    currentPolygon.width = Size.Value;
+                    currentPolygon.Draw(MousedrawingCanvas, false);
+                }
+                else if (e.RightButton == MouseButtonState.Pressed && isDrawingPolygon)
+                {
+                    // 右击鼠标 开始涂色多边形
+                    currentPolygon.Draw(MousedrawingCanvas, true);
+
+                    // 重新初始化
+                    save_Polygon.PushPolygon(currentPolygon);
+                    currentPolygon = new Polygon();
+                    isDrawingPolygon = false;
+                    if (temPath != null)
+                    {
+                        MousedrawingCanvas.Children.Remove(temPath);
+                    }
+
+
+                }
+
+            }
+        }
+
+        private void MousedrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            // 获取鼠标的当前位置
+            double newX = e.GetPosition(MousedrawingCanvas).X;
+            double newY = e.GetPosition(MousedrawingCanvas).Y;
+
+            // 更新TextBox的文本为鼠标的实时位置
+            string formattedText = $"X: {newX:F4}, Y: {newY:F4}";
+            MouseTextBox.Text = formattedText;
+
+            //橡皮筋效果
+            if ((Item_Polygon.IsSelected == true || Item_Line.IsSelected == true) && (isDrawingLine || isDrawingPolygon) && (currentLine != null || currentPolygon != null))
+            {
+                double winX = e.GetPosition(MousedrawingCanvas).X;
+                double winY = e.GetPosition(MousedrawingCanvas).Y;
+                LineGeometry myLineGeometry = new LineGeometry();
+                myLineGeometry.StartPoint = new System.Windows.Point(currentPoint.x, currentPoint.y);
+                myLineGeometry.EndPoint = new System.Windows.Point(winX, winY);
+
+                if (!MousedrawingCanvas.Children.Contains(temPath))
+                {
+                    temPath.Stroke = new SolidColorBrush((Color)DrawcolorPicker.SelectedColor);
+                    temPath.StrokeThickness = Size.Value;
+                    MousedrawingCanvas.Children.Add(temPath);
+                }
+
+                temPath.Data = myLineGeometry;
+            }
+
+            if (e.LeftButton == MouseButtonState.Pressed && isDragging)
+            {
+                // 计算鼠标位置的变化量
+                Double newPositionX = e.GetPosition(MousedrawingCanvas).X;
+                Double newPositionY = e.GetPosition(MousedrawingCanvas).Y;
+                Point newPosition = new Point(newPositionX, newPositionY);
+                Vector delta = new Vector();
+                delta.X = newPosition.x - lastPosition.x;
+                delta.Y = newPosition.y - lastPosition.y;
+
+                Translate(delta);
+
+                // 更新鼠标位置
+                lastPosition = newPosition;
+            }
+            else if (e.RightButton == MouseButtonState.Pressed && isDrawingPolygon)
+            {
+                // 右击鼠标 开始涂色多边形
+                currentPolygon.Draw(MousedrawingCanvas, true);
+
+                // 重新初始化
+                save_Polygon.PushPolygon(currentPolygon);
+                currentPolygon = new Polygon();
+                isDrawingPolygon = false;
+                if (temPath != null)
+                {
+                    MousedrawingCanvas.Children.Remove(temPath);
+                }
+
+
+            }
+        }
+        private void MousedrawingCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isDragging)
+            {
+                MousedrawingCanvas.ReleaseMouseCapture();
+            }
+        }
+        private void Translate(Vector translation)
+        {
+            MousedrawingCanvas.Children.Clear();
+            foreach (Line line in save_Line.m_multiLine)
+            {
+                line.MaxY = -9999;
+                line.MaxX = -9999;
+                line.MinY = 9999;
+                line.MinX = 9999;
+                for (int i = 0; i < line.m_Line.Count; i++)
+                {
+                    line.m_Line[i].x += translation.X;
+                    line.m_Line[i].y += translation.Y;
+                }
+                line.Draw(MousedrawingCanvas);
+
+            }
+
+            foreach (Polygon polygon in save_Polygon.m_multiPolygon)
+            {
+                polygon.MaxY = -9999;
+                polygon.MaxX = -9999;
+                polygon.MinY = 9999;
+                polygon.MinX = 9999;
+                for (int i = 0; i < polygon.m_polygon.Count; i++)
+                {
+                    polygon.m_polygon[i].x += translation.X;
+                    polygon.m_polygon[i].y += translation.Y;
+                }
+                polygon.Draw(MousedrawingCanvas, true);
+            }
+
+            foreach (Point point in save_Point.m_multiPoint)
+            {
+                point.x += translation.X;
+                point.y += translation.Y;
+                point.Draw(MousedrawingCanvas);
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Checkbox_Edit_Checked(object sender, RoutedEventArgs e)
+        {
+            Item_Point.IsEnabled = true;
+            Item_Line.IsEnabled = true;
+            Item_Polygon.IsEnabled = true;
+            isDrawingEnabled = true;
+        }
+
+        private void Checkbox_Edit_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Item_Point.IsEnabled = false;
+            Item_Line.IsEnabled = false;
+            Item_Polygon.IsEnabled = false;
+            isDrawingEnabled = false;
+        }
+
+        //private void Slider_PointSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    ComboBox_PointSize.Text = Slider_PointSize.Value.ToString();
+        //}
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //更改画点的半径
+            currentPoint.radius = Size.Value;
+            //更改画线宽
+            currentLine.width = Size.Value;
+            currentPolygon.width = Size.Value;
+        }
+        private void ListBoxItem_Goodbye_Selected(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Item_Point_Selected(object sender, RoutedEventArgs e)
+        {
+            Size.Value = 5;
+            Size.Minimum = 1;
+            Size.Maximum = 50;
+        }
+        private void Item_Line_Selected(object sender, RoutedEventArgs e)
+        {
+            Size.Value = 1;
+            Size.Minimum = 1;
+            Size.Maximum = 10;
+        }
+        private void Item_Polygon_Selected(object sender, RoutedEventArgs e)
+        {
+            Size.Value = 1;
+            Size.Minimum = 1;
+            Size.Maximum = 10;
+        }
+        private void WrappedRectangle(object sender, RoutedEventArgs e)
+        {
+            foreach (Line line in save_Line.m_multiLine)
+            {
+                // 获取四个角的位置
+                double left = line.MinX;
+                double top = line.MinY;
+                double right = line.MaxX;
+                double bottom = line.MaxY;
+
+                // 计算矩形框的位置和尺寸
+                double width = right - left;
+                double height = bottom - top;
+
+                // 创建一个矩形框对象
+                Rectangle rectangle = new Rectangle();
+
+                // 设置矩形框的位置和尺寸
+                rectangle.Margin = new Thickness(left, top, 0, 0);
+                rectangle.Width = width;
+                rectangle.Height = height;
+
+                // 设置矩形框的边框样式和颜色
+                rectangle.Stroke = Brushes.Red;
+                rectangle.StrokeThickness = 2;
+
+                // 将矩形框添加到Canvas控件中
+                MousedrawingCanvas.Children.Add(rectangle);
+
+            }
+
+            foreach (Polygon polygon in save_Polygon.m_multiPolygon)
+            {
+                // 获取四个角的位置
+                double left = polygon.MinX;
+                double top = polygon.MinY;
+                double right = polygon.MaxX;
+                double bottom = polygon.MaxY;
+
+                // 计算矩形框的位置和尺寸
+                double width = right - left;
+                double height = bottom - top;
+
+                // 创建一个矩形框对象
+                Rectangle rectangle = new Rectangle();
+
+                // 设置矩形框的位置和尺寸
+                rectangle.Margin = new Thickness(left, top, 0, 0);
+                rectangle.Width = width;
+                rectangle.Height = height;
+
+                // 设置矩形框的边框样式和颜色
+                rectangle.Stroke = Brushes.Red;
+                rectangle.StrokeThickness = 2;
+
+                // 将矩形框添加到Canvas控件中
+                MousedrawingCanvas.Children.Add(rectangle);
+            }
+        }
+        private void PaintColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if (e.NewValue.HasValue)
+            {
+                Color selectedColor = e.NewValue.Value;
+                // 你可以在这里处理所选的颜色，例如更改背景色等。
+            }
+        }
+        private void TranslateButton(object sender, RoutedEventArgs e)
+        {
+            isDrawingEnabled = false;
+            Temp = 1 - Temp;
+            if (Temp == 1)
+            {
+                (sender as Button).Content = "停止平移";
+                isDragging = true;
+
+            }
+            else
+            {
+                (sender as Button).Content = "开始平移";
+                isDragging = false;
+            }
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if (e.NewValue.HasValue)
+            {
+                Color selectedColor = e.NewValue.Value;
+                // 你可以在这里处理所选的颜色，例如更改背景色等。
+            }
+        }
+
+        private void Item_Cursor_Selected(object sender, RoutedEventArgs e)
+        {
+            isDrawingEnabled = true;
+            isDragging = false;
+        }
+
+        private void Item_Hand_Selected(object sender, RoutedEventArgs e)
+        {
+            isDragging = true;
+            isDrawingEnabled = false;
+        }
+
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
